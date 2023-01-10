d348d029974b551e05cd73591c445cb9afd72f14
unknown <yung.chun.kau@gmail.com>
2023-01-11 06:05:54 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace zip2;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+		try
+		{
+            RunMain(args);
+		}
+		catch (Exception ee)
+		{
+			Console.WriteLine(ee);
+		}
+    }
+
+    static public void RunMain(string[] args)
+    {
+        Console.WriteLine($"RunMain #args:{args.Length}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# zip2
+**v2.0.0**
+
+2021, 2023 (c) Yung, Chun Kau
+
+yung.chun.kau@gmail.com
